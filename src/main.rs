8484fd34267cb2c98a3b520f0dd09794d95edad1
ejaszczuk <emj1054@gmail.com>
2021-11-03 21:02:26 +0100

    Add basic list

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,9 @@ use std::{thread, time};
 use termion::{async_stdin, event::Key, input::TermRead, raw::IntoRawMode};
 use tui::backend::TermionBackend;
 use tui::layout::{Constraint, Direction, Layout};
-use tui::style::{Color, Style};
+use tui::style::{Color, Modifier, Style};
 use tui::text::Spans;
-use tui::widgets::{Block, Borders, Paragraph};
+use tui::widgets::{Block, Borders, List, ListItem, ListState, Paragraph};
 use tui::Terminal;
 
 mod foldertree;
@@ -84,24 +84,74 @@ impl MyEndpointOrFolder {
     }
 }
 
+struct MyEvents {
+    items: Vec<String>,
+    state: ListState,
+}
+impl MyEvents {
+    fn new(items: Vec<String>) -> MyEvents {
+        MyEvents {
+            items,
+            state: ListState::default(),
+        }
+    }
+
+    pub fn set_items(&mut self, items: Vec<String>) {
+        self.items = items;
+        self.state = ListState::default();
+    }
+
+    pub fn next(&mut self) {
+        let i = match self.state.selected() {
+            Some(i) => {
+                if i >= self.items.len() - 1 {
+                    0
+                } else {
+                    i + 1
+                }
+            }
+            None => 0,
+        };
+        self.state.select(Some(i));
+    }
+
+    pub fn previous(&mut self) {
+        let i = match self.state.selected() {
+            Some(i) => {
+                if i == 0 {
+                    self.items.len() - 1
+                } else {
+                    i - 1
+                }
+            }
+            None => 0,
+        };
+        self.state.select(Some(i));
+    }
+
+    pub fn unselect(&mut self) {
+        self.state.select(None);
+    }
+}
+
 fn main() -> Result<(), io::Error> {
     // Set up terminal output
-    // let stdout = io::stdout().into_raw_mode()?;
-    // let backend = TermionBackend::new(stdout);
-    // let mut terminal = Terminal::new(backend)?;
+    let stdout = io::stdout().into_raw_mode()?;
+    let backend = TermionBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
 
-    // // Create a separate thread to poll stdin.
-    // // This provides non-blocking input support.
-    // let mut asi = async_stdin();
+    // Create a separate thread to poll stdin.
+    // This provides non-blocking input support.
+    let mut asi = async_stdin();
 
-    // let app = Arc::new(Mutex::new(AppState::new()));
-    // let cloned_app = Arc::clone(&app);
-    // let (tx, rx) = std::sync::mpsc::channel();
+    let app = Arc::new(Mutex::new(AppState::new()));
+    let cloned_app = Arc::clone(&app);
+    let (tx, rx) = std::sync::mpsc::channel();
 
-    // thread::spawn(move || loop {
-    //     do_thread_stuff(&app, &rx);
-    //     // thread::sleep(time::Duration::from_millis(100));
-    // });
+    thread::spawn(move || loop {
+        do_thread_stuff(&app, &rx);
+        // thread::sleep(time::Duration::from_millis(100));
+    });
     let j = r#"
 [
     {
@@ -141,100 +191,94 @@ fn main() -> Result<(), io::Error> {
 ]
     "#;
 
-    let k = r#"
-[
-    {
-        "type": "folder",
-        "name": "Pierwszy",
-        "folded": false,
-        "items": [
-            {
-                "type": "endpoint",
-                "name": "Dodaj usera",
-                "method": "POST"
-            },
-            {
-                "type": "endpoint",
-                "name": "Zmien userow",
-                "method": "PUT"
-            }
-        ]
-    },
-    {
-        "type": "endpoint",
-        "name": "Costam",
-        "method": "POST"
-    }
-]
-    "#;
-
-    // let p: Vec<MyEndpointOrFolder> = serde_json::from_str(&k).expect("xd");
-    // println!("{:#?}", p);
-
     let mut ftc = FolderTreeComponent::new();
     ftc.from_str(j);
 
+    let mut costam: Vec<String> = Vec::new();
+
     for item in ftc.items {
         println!("{:#?}", item.obj);
+        costam.push(item.rep);
     }
 
-    Ok(())
+    let mut events = MyEvents::new(costam);
 
-    // // Clear the terminal before first draw.
-    // terminal.clear()?;
-    // loop {
-    //     let mut app = cloned_app.lock().unwrap();
+    // Clear the terminal before first draw.
+    terminal.clear()?;
+    loop {
+        let mut app = cloned_app.lock().unwrap();
 
-    //     // Lock the terminal and start a drawing session.
-    //     terminal.draw(|frame| {
-    //         // Create a layout into which to place our blocks.
-    //         let chunks = Layout::default()
-    //             .direction(Direction::Horizontal)
-    //             .constraints([Constraint::Percentage(20), Constraint::Percentage(80)].as_ref())
-    //             .split(frame.size());
+        // Lock the terminal and start a drawing session.
+        terminal.draw(|frame| {
+            // Create a layout into which to place our blocks.
+            let chunks = Layout::default()
+                .direction(Direction::Horizontal)
+                .constraints([Constraint::Percentage(30), Constraint::Percentage(70)].as_ref())
+                .split(frame.size());
 
-    //         // Create a block...
-    //         let block = Block::default()
-    //             // With a given title...
-    //             .title("Color Changer")
-    //             // Borders on every side...
-    //             .borders(Borders::ALL)
-    //             // The background of the current color...
-    //             .style(Style::default());
+            // // Create a block...
+            // let block = Block::default()
+            //     // With a given title...
+            //     .title("Color Changer")
+            //     // Borders on every side...
+            //     .borders(Borders::ALL)
+            //     // The background of the current color...
+            //     .style(Style::default());
 
-    //         // Render into the first chunk of the layout.
-    //         frame.render_widget(block, chunks[0]);
+            let items: Vec<ListItem> = events
+                .items
+                .iter()
+                .map(|i| ListItem::new(i.as_ref()))
+                .collect();
 
-    //         // The text lines for our text box.
-    //         let txt = vec![Spans::from(format!("{}", app.count))];
-    //         // Create a paragraph with the above text...
-    //         let graph = Paragraph::new(txt)
-    //             // In a block with borders and the given title...
-    //             .block(Block::default().title("Text box").borders(Borders::ALL))
-    //             // With white foreground and black background...
-    //             .style(Style::default().fg(Color::White).bg(Color::Black));
+            let block = List::new(items)
+                .block(Block::default().title("List").borders(Borders::ALL))
+                .style(Style::default().fg(Color::Rgb(180, 180, 180)))
+                .highlight_style(
+                    Style::default()
+                        .fg(Color::White)
+                        .add_modifier(Modifier::BOLD),
+                )
+                .highlight_symbol(">");
 
-    //         // Render into the second chunk of the layout.
-    //         frame.render_widget(graph, chunks[1]);
-    //     })?;
+            // Render into the first chunk of the layout.
+            frame.render_stateful_widget(block, chunks[0], &mut events.state);
 
-    //     // Iterate over all the keys that have been pressed since the
-    //     // last time we checked.
-    //     for k in asi.by_ref().keys() {
-    //         match k.unwrap() {
-    //             // If any of them is q, quit
-    //             Key::Char('q') => {
-    //                 // Clear the terminal before exit so as not to leave
-    //                 // a mess.
-    //                 terminal.clear()?;
-    //                 return Ok(());
-    //             }
-    //             Key::Char('e') => {
-    //                 tx.send(10).unwrap();
-    //             }
-    //             // Otherwise, throw them away.
-    //             _ => (),
-    //         }
-    //     }
-    // }
+            // The text lines for our text box.
+            let txt = vec![Spans::from(format!("{}", app.count))];
+            // Create a paragraph with the above text...
+            let graph = Paragraph::new(txt)
+                // In a block with borders and the given title...
+                .block(Block::default().title("Text box").borders(Borders::ALL))
+                // With white foreground and black background...
+                .style(Style::default().fg(Color::White).bg(Color::Black));
+
+            // Render into the second chunk of the layout.
+            frame.render_widget(graph, chunks[1]);
+        })?;
+
+        // Iterate over all the keys that have been pressed since the
+        // last time we checked.
+        for k in asi.by_ref().keys() {
+            match k.unwrap() {
+                // If any of them is q, quit
+                Key::Char('q') => {
+                    // Clear the terminal before exit so as not to leave
+                    // a mess.
+                    terminal.clear()?;
+                    return Ok(());
+                }
+                Key::Char('s') => {
+                    // tx.send(10).unwrap();
+                    events.next();
+                }
+                Key::Char('w') => {
+                    // tx.send(10).unwrap();
+                    events.previous();
+                }
+                // Otherwise, throw them away.
+                _ => (),
+            }
+        }
+    }
 }
